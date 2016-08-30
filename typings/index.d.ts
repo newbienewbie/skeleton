ecaf843354139e78482249c630eae47db5263bcf
newbienewbie <webbot@sina.com>
2016-08-31 00:18:27 +0800

    安装fluent-ffmpeg

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,6 +2,7 @@
 /// <reference path="globals/bcryptjs/index.d.ts" />
 /// <reference path="globals/body-parser/index.d.ts" />
 /// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/fluent-ffmpeg/index.d.ts" />
 /// <reference path="globals/multer/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/nodemailer/index.d.ts" />
