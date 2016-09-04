fb0d380da1f9b27792c6b26b6b464bd57c17fe13
newbienewbie <webbot@sina.com>
2016-09-05 01:01:09 +0800

    修复前端组件UEditor在同一页面的再次加载无效的bug
    组件卸载之前，使用UE.delEditor(id)的方法移除已经创建的editor。

--- lib/frontend/src/js/admin/Post/add.jsx
+++ lib/frontend/src/js/admin/Post/add.jsx
@@ -8,7 +8,7 @@ const Add=React.createClass({
         return (<div className="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main">
             <form action="" method='post' className="container" >
                 <input name='title'/>
-                <Ueditor id="ueditorContainer" name="content" width="800" height="500" />
+                <Ueditor id="ueditorContainer" name="content" width={800} height={500} />
                 <input className="btn btn-warning" type='submit' name="提交" value='提交'/>
             </form>
         </div>);
--- lib/frontend/src/js/admin/Post/ueditor.jsx
+++ lib/frontend/src/js/admin/Post/ueditor.jsx
@@ -7,17 +7,21 @@ const Ueditor = React.createClass({
         return{
             id:'ueditorcontainer',
             name:'uecontent',
-            height:'600',    // 注意这里只能是数字，不可有单位
-            width:'600',     // 注意这里只能是数字，不可有单位
+            height:600,    // 注意这里只能是数字，不可有单位
+            width:600,     // 注意这里只能是数字，不可有单位
         };
     },
 
     componentDidMount() {
-        const that=this;
-        const editor = UE.getEditor(this.props.id,{
+        document.getElementById(this.props.id).innerHTML="";
+        let ue=UE.getEditor(this.props.id,{
             initialFrameWidth: this.props.width,
             initialFrameHeight: this.props.height,
         });
+    },
+
+    componentWillUnmount(){
+        UE.delEditor(this.props.id);
     },
     
     render: function () {
@@ -25,7 +29,7 @@ const Ueditor = React.createClass({
             <script id={this.props.id} name={this.props.content}
                 type="text/plain">
             </script>
-        )
+        );
     }
 
 });
