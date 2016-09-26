20cd10b0ddba3be51521129696ee8b898d78998f
newbienewbie <webbot@sina.com>
2016-09-27 00:50:20 +0800

    前端：后台：增加角色管理

--- lib/frontend/src/js/admin/user/list.jsx
+++ lib/frontend/src/js/admin/user/list.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Table,Pagination,message} from 'antd';
+import {Table,Card,Transfer,Button,Pagination,message} from 'antd';
 import 'whatwg-fetch';
 
 
-
+/**
+ * 用户列表
+ */
 const List=React.createClass({
 
     getDefaultProps:function(){
@@ -13,11 +15,11 @@ const List=React.createClass({
                 {title:'id',dataIndex:'id'},
                 {title:'用户名',dataIndex:'username'},
                 {title:'email',dataIndex:'email'},
-                {title:'角色',dataIndex:'roles'},
                 {title:'状态',dataIndex:'state'},
                 {title:'创建时间',dataIndex:'createdAt'},
                 {title:'修改时间',dataIndex:'updatedAt'},
             ],
+            onRowClick:(record,index)=>{},
         };
     },
 
@@ -43,7 +45,6 @@ const List=React.createClass({
                     total:info.count,
                     rows:info.rows
                 },
-                ()=>{console.log(this.state)}
             );
         })
         .catch(e=>{
@@ -58,7 +59,7 @@ const List=React.createClass({
     render:function(){
         return (<div className="container">
             <div>
-                <Table dataSource={this.state.rows} columns={this.props.columns} pagination={false}/>
+                <Table onRowClick={this.props.onRowClick} dataSource={this.state.rows} columns={this.props.columns} pagination={false}/>
             </div>
             <Pagination total={this.state.total} current={this.state.current}  pageSize={2}
                 onChange={(v)=>{
@@ -74,5 +75,111 @@ const List=React.createClass({
 
 
 
+/**
+ * 角色管理
+ */
+const RoleManager=React.createClass({
 
-module.exports=List;
+    getInitialState:function(){
+        return {
+            dataSource:[],
+            targetKeys:[],
+            onChange:()=>{},
+        };
+    },
+
+    getDefaultProps:function(){
+        return { 
+            title:'角色管理',
+            bordered:false,
+            record:{}, 
+        };
+    },
+
+    fetchAllRoles:function(){
+        return fetch('/role/list')
+            .then(resp=>resp.json())
+            .then(roles=>{
+                this.setState({
+                    dataSource:roles.map(r=>{
+                        return {
+                            key:r.id,
+                            id:r.id,
+                            title:r.name,
+                            description:r.description,
+                        };
+                    }),
+                });
+            })
+    },
+
+    componentDidMount:function(){
+        this.fetchAllRoles();
+    },
+    /**
+     * 当收到属性时，
+     *     1. 更新当前用户对应的targetKeys：载入新属性中的记录的roles 到当前state
+     *     2. 重置dataSource
+     */
+    componentWillReceiveProps:function(nextProps){
+        if(nextProps.record.username==this.props.record.username){
+            return;
+        }else{
+            let roles=nextProps.record.roles;
+            let targetKeys=typeof roles =="object"? roles: JSON.parse(roles);
+            Promise.all([
+                this.setState({ targetKeys}),
+                this.fetchAllRoles()
+            ]);
+        }
+    },
+
+    render:function(){
+        return (<Card title={this.props.title} bordered={this.props.bordered}  >
+
+            <div>
+                当前角色名：{this.props.record.username}
+            </div>
+
+            <Transfer titles={['角色池','赋予的角色']}
+                dataSource={this.state.dataSource} 
+                targetKeys={this.state.targetKeys} 
+                render={item=>item.title}
+                onChange={(targetKeys)=>{this.setState({targetKeys})}}
+            />
+
+            <div>
+                <Button type="primary" onClick={()=>{
+                    console.log(this.state.targetKeys);
+                }}>
+                    确定
+                </Button>
+            </div>
+        </Card>);
+    },
+});
+
+
+
+/**
+ * 列表包装器
+ */
+const ListWrapper=React.createClass({
+    getInitialState:function(){
+        return {
+            record:{},
+        };
+    },
+    render:function(){
+        return (<div>
+            <List onRowClick={(record,index)=>{
+                this.setState({record});
+            }} />
+            <RoleManager record={this.state.record}/>
+        </div>);
+    }
+});
+
+
+
+module.exports=ListWrapper;
