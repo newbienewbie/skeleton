30278691427f8ef7b3c5b902ce85bd75b53a84ec
newbienewbie <webbot@sina.com>
2016-09-29 00:55:39 +0800

    前端:安装:添加安装的前台
    home.jsx 为安装界面；
    request.js 为每一步的请求过程；

--- lib/frontend/src/js/install/home.jsx
+++ lib/frontend/src/js/install/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {Card,Row,Col,Button,Steps} from 'antd';
+import {Card,Row,Col,Button,Steps,Form,Input,Alert} from 'antd';
 import Request from './request.js';
 
 
@@ -10,16 +10,66 @@ const Home=React.createClass({
         return {
             current:0,
             status:'wait',
+            msg:'',
+            rootUser:{
+                username:'',
+                password:'',
+                email:'',
+            },
         };
     },
 
+    promiseSetState:function(state){
+        this.setState(state,()=>{
+            console.log(`已经进行到step: ${this.state.current} `);
+            return Promise.resolve(this.state);
+        });
+    },
+
     install:function(){
-        
+        // 依次安装 ，设置状态
+        this.setState({status:'process'},()=>{
+            let current=0;
+            // 创建数据库
+            Request.createDb()
+                .then(()=>{
+                    //完成后，进入下一步骤
+                    return this.promiseSetState({current:++current},);
+                })
+            // 创建根用户
+            .then(
+                ()=>{
+                    const rootUser=this.state.rootUser;
+                    return Request.createRootUser(rootUser.username,rootUser.password,rootUser.email);
+                }
+            )
+                .then(()=>{
+                    return this.promiseSetState({current:++current});
+                })
+            .catch(e=>{
+                console.log(this.state);
+                return this.setState({ status:'error',current:current,msg:e.msg});
+            });
+        });
+
     },
 
     render: function () {
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
         return (
         <div>
+            <Row>
+                <Col md={18}>
+                    <Form horizontal>
+                        <Form.Item label="帐户名" {...formItemLayout}  >
+                            <Input onChange={()=>{}} />
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
             <Row>
                 <Col md={4} > 安装网站： </Col>
                 <Col md={18}>
@@ -28,6 +78,7 @@ const Home=React.createClass({
             </Row>
             <Row>
                 <Col md={18}>
+                    <Alert message={this.state.msg} type={this.state.status=='error'?'error':'info'} />
                     <Card title="安装进度">
                         <Steps direction="vertical" current={this.state.current} status={this.state.status}>
                             <Steps.Step title="安装数据库" description="数据库安装" />
