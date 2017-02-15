eece874edddc58cb2fc952d7ecb89cc7c10853f4
newbienewbie <webbot@sina.com>
2017-02-16 01:22:12 +0800

    前端：联系表单调整

--- lib/frontend/src/js/front-desk/contact/index.jsx
+++ lib/frontend/src/js/front-desk/contact/index.jsx
@@ -27,30 +27,37 @@ export const Contact = React.createClass({
             </div>
             <div className="feedback" >
                 <div className="feedback-form">
-                    <h2>FEEDBACK</h2>
+                    <h2>反馈您的意见</h2>
                     <form>
-                        <label>NAME</label>
-                        <input value="" id="name" type="text" />
-                        <label>E-MAIL</label>
-                        <input value="" type="text" />
-                        <label>SUBJECT</label>
-                        <textarea ></textarea>
-                        <button type="submit">提交</button>
+                        <div>
+                            <label>姓名</label>
+                            <input value="" id="name" type="text" />
+                        </div>
+                        <div>
+                            <label>邮箱</label>
+                            <input value="" type="text" />
+                        </div>
+                        <div>
+                            <label>内容</label>
+                            <textarea ></textarea>
+                        </div>
+                        <div>
+                            <button type="submit">提交</button>
+                        </div>
                     </form>
                 </div>
                 <div className="feedback-info">
-                    <h2>CONTACT-INFO</h2>
+                    <h2>联系信息</h2>
                     <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
                         Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced
                         in their exact original form, accompanied by English versions from the 1914 translation by
                         H. Rackham.</p>
                     <address>
-                        <strong>The Company Name Inc.
-                        9870 St Vincent Place,<br/>
-                        Glasgow, DC 45 Fr 45.</strong><br/> 
-                        Telephone: +1 800 603 6035<br/> 
-                        FAX: +1 800 889 9898<br/> 
-                        E-mail: <a href="mailto:info@example.com">mail@example.com </a><br/>
+                        <strong>The Company Name Inc.<br/>
+                        9870 St Vincent Place, Glasgow, DC 45 Fr 45.</strong><br/> 
+                        电话: +1 800 603 6035<br/> 
+                        传真: +1 800 889 9898<br/> 
+                        邮箱: <a href="mailto:info@example.com">mail@example.com </a><br/>
                     </address>
                 </div>
             </div>
