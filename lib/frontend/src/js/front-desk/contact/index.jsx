26fd84c089ff38f24699968500b9e29d82c74028
newbienewbie <webbot@sina.com>
2017-02-15 23:22:56 +0800

    更改contact模板，增加联系信息

--- lib/frontend/src/js/front-desk/contact/index.jsx
+++ lib/frontend/src/js/front-desk/contact/index.jsx
@@ -1,36 +1,58 @@
 import React from 'react';
-import {BMapComponent} from './bmap.jsx';
+import { BMapComponent } from './bmap.jsx';
 import './style.less';
 
 
-export const Contact=React.createClass({
+export const Contact = React.createClass({
 
 
-    render:function(){
+    render: function () {
         return (<div className="contact">
-            <div className="feedback">
-                <h1>FEEDBACK</h1>
-                <form>
-                    <label>NAME</label>
-                    <input value="" id="name" type="text"/>
-                    <label>E-MAIL</label>
-                    <input value="" type="text"/>
-                    <label>SUBJECT</label>
-                    <textarea ></textarea>
-                    <button type="submit">提交</button>
-                </form>
-            </div>
+            <p>
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections
+                    1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact
+                    original form, accompanied by English versions from the 1914 translation by H. Rackham.
+            </p>
             <div className="map">
-                <BMapComponent 
-                    ak={"kdoHd3T6c9rQqdxSePGlydrAGAOyDGIv"}  
-                    callback={(map)=>{
+                <BMapComponent
+                    ak={"kdoHd3T6c9rQqdxSePGlydrAGAOyDGIv"}
+                    callback={(map) => {
                         var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
                         var bs = map.getBounds();   //获取可视区域
                         var bssw = bs.getSouthWest();   //可视区域左下角
                         var bsne = bs.getNorthEast();   //可视区域右上角
-                        map.centerAndZoom(point, 15);  
+                        map.centerAndZoom(point, 15);
                     }}
                 />
+            </div>
+            <div className="feedback" >
+                <div className="feedback-form">
+                    <h2>FEEDBACK</h2>
+                    <form>
+                        <label>NAME</label>
+                        <input value="" id="name" type="text" />
+                        <label>E-MAIL</label>
+                        <input value="" type="text" />
+                        <label>SUBJECT</label>
+                        <textarea ></textarea>
+                        <button type="submit">提交</button>
+                    </form>
+                </div>
+                <div className="feedback-info">
+                    <h2>CONTACT-INFO</h2>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                        Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced
+                        in their exact original form, accompanied by English versions from the 1914 translation by
+                        H. Rackham.</p>
+                    <address>
+                        <strong>The Company Name Inc.
+                        9870 St Vincent Place,<br/>
+                        Glasgow, DC 45 Fr 45.</strong><br/> 
+                        Telephone: +1 800 603 6035<br/> 
+                        FAX: +1 800 889 9898<br/> 
+                        E-mail: <a href="mailto:info@example.com">mail@example.com </a><br/>
+                    </address>
+                </div>
             </div>
         </div>);
     }
