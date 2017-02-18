a0df120f5ef5ea2f1d92dafe46ec76c03f38db03
newbienewbie <webbot@sina.com>
2017-02-18 15:06:36 +0800

    前端：文章首页现在从服务器加载数据了（不再是模拟数据）

--- lib/frontend/src/js/front-desk/post/home/index.jsx
+++ lib/frontend/src/js/front-desk/post/home/index.jsx
@@ -1,23 +1,66 @@
 import React from 'react';
 import {Item} from './item.jsx';
 import './style.less';
+import 'whatwg-fetch';
 
 
 const Home=React.createClass({
+
+
+    getInitialState:function(){
+        return {
+            posts:[
+                {
+                    title:'白雪飞天横舟渡', 
+                    author:'itminus', 
+                    authorUrl:'http://www.itminus.com', 
+                    publishedAt:new Date(), 
+                    imageUrl:'/static/img/react-web.png', 
+                    detailUrl:'http://www.itminus.com', 
+                    excerpt:'床前明月光，疑是地上霜。举头望明月，低头思故乡。' 
+                },
+            ],
+        };
+    },
+
+    componentDidMount:function(){
+        fetch('/post/recent',{
+            method:'post',
+            credentials:'same-origin',
+            body:JSON.stringify({
+                page:1,
+                size:8,
+            }),
+        }).then(resp=>resp.json())
+        .then(list=>{
+            return list.map(i=>{
+                // 客户端路由
+                const detailUrl=`/#/post/detail/${i.id}`;
+                return {
+                    id:i.id,
+                    title:i.title,
+                    author:i.author.username,
+                    authorUrl:i.author.username,
+                    imageUrl:'#',
+                    detailUrl,
+                    excerpt:i.excerpt,
+                    publishedAt:new Date(),
+                };
+            });
+        })
+        .then(list=>{
+            this.setState({posts:list});
+        })
+    },
+
     render:function () {
         return (<article id="article-container">
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {this.state.posts.map((p,i)=>{
+                return <Item key={i} title={p.title} publishedAt={p.publishedAt}
+                    author={p.author} authorUrl={p.authorUrl} 
+                    imageUrl={p.imageUrl} detailUrl={p.detailUrl}
+                    excerpt={p.excerpt} />;
+            })}
         </article>);
     }
 });
