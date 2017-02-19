5620fa7390c40c69004edb2a3de4f111e1d44b5b
newbienewbie <webbot@sina.com>
2017-02-19 22:22:43 +0800

    后端：服务：更新创建文章服务及其测试

---
+++ test/backend/service/post/index.test.js
@@ -0,0 +1,49 @@
+const assert=require('assert');
+const postService=require('../../../../lib/backend/service/post');
+
+
+describe('测试post服务',function(){
+    it('测试create()',function(){
+        return postService.create({
+            title:'标题',
+            categoryId:1,
+            authorId:1,
+            content:'测试',
+            excerpt:'摘要',
+            keywords:[
+                {tag:'标签5'},
+                {tag:'标签6'},
+            ]
+        })
+        // 断言检查
+        .then(p=>{
+            assert.equal(p.title,"标题");
+            assert.equal(p.content,"测试");
+            assert.equal(p.excerpt,"摘要");
+            assert.equal(p.categoryId,1);
+            assert.equal(p.authorId,1);
+            return p.getKeywords().then(keywords=>{
+                keywords.forEach(k=>{
+                    assert.equal(k.postId,p.id,`关键词的postId和文章的id必须相等：${k.postId}-${p.id}`)
+                });
+            })
+            // 返回以留待删除
+            .then(_=>{
+                return p;
+            });
+        })
+        // 删除
+        .then(p=>{
+            return p.getKeywords()
+                .then(keywords=>keywords.map(k=>{
+                    return k.destroy();
+                }))
+                .then(_=>{
+                    return p.destroy();
+                });
+        })
+        .catch(err=>{
+            assert.fail(err);
+        });
+    });
+});
