d57f8ba1e1965f5fd8a15f26e4e851ae95c07328
newbienewbie <webbot@sina.com>
2017-02-25 21:20:04 +0800

    前端：为文章评论界面添加分页

--- lib/frontend/src/js/front-desk/post/detail/index.jsx
+++ lib/frontend/src/js/front-desk/post/detail/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Article} from './article.jsx';;
 import Comment  from './comment/index.jsx';
+import Pagination from 'simple-react-ui/dist/pagination';
 import 'whatwg-fetch';
 
 const {CommentForm,CommentList} =Comment;
@@ -13,17 +14,21 @@ const Detail=React.createClass({
                 {id:1,author:{name:'xx1',email:'',avatarUrl:'#',introduction:'一句话掐死你',},content:'balabala',createdAt:new Date(),updatedAt:new Date(),upvotes:0,downvotes:0,},
                 {id:2,author:{name:'xx2',email:'',avatarUrl:'#',introduction:'一句话饿死你',},content:'balabalabalabalabalabalabalabalabalabalabalabala',createdAt:new Date(),updatedAt:new Date(),upvotes:0,downvotes:0,},
             ],
+            total:0,
             page:1,
             size:10,
         };
     },
 
     componentDidMount:function(){
-        this.fetchCommentList();
+        this.fetchCommentList(1,8).then((result)=>{
+            const {comments,total}=result;
+            this.setState({comments,total});
+        });
     },
 
-    fetchCommentList:function(){
-        fetch(`/comment/list`,{
+    fetchCommentList:function(page=1,size=8){
+        return fetch(`/comment/list`,{
             method:'post',
             credentials:'same-origin',
             headers:{
@@ -31,8 +36,8 @@ const Detail=React.createClass({
             },
             body:JSON.stringify({
                 topicId:this.props.params.id,
-                page:this.state.page,
-                size:this.state.size,
+                page,
+                size,
             }),
         })
         .then(resp=>resp.json())
@@ -41,7 +46,9 @@ const Detail=React.createClass({
                 c.author.name=c.author.username;
                 return c;
             });
-            this.setState({comments});
+            console.log(`fff`,comments);
+            const total=result.count;
+            return {comments,total};
         });
     },
 
@@ -57,15 +64,29 @@ const Detail=React.createClass({
                     body:JSON.stringify({
                         content:value,
                         topicId:this.props.params.id,
+                        page:1,
+                        size:this.state.size,
                     })
                 })
                 .then(resp=>resp.json())
                 .then(info=>{
                     console.log(info);
-                    this.fetchCommentList();
+                    return this.fetchCommentList();
+                }).then((result)=>{
+                    const {comments,total}=result;
+                    this.setState({comments,total});
                 });
             }} />
             <CommentList comments={this.state.comments}/>
+            <Pagination current={this.state.page} size={this.state.size} total={this.state.total} 
+                onChange={(page)=>{
+                    this.fetchCommentList(page,this.state.size)
+                        .then(result=>{
+                            const {comments,total}=result;
+                            this.setState({ comments,total,page });
+                        });
+                }}
+            />
         </div>);
     }
 });
