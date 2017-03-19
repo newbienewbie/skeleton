3fc423495fdc772ce447c82897dc40cd9c0a83fe
newbienewbie <webbot@sina.com>
2017-03-19 16:03:28 +0800

    后端服务：添加列表转为树的服务及其测试

---
+++ test/backend/service/category/tree.test.js
@@ -0,0 +1,30 @@
+const assert=require('assert');
+const {listToTree}=require('../../../../lib/backend/service/category/tree');
+
+
+describe("测试 tree.js",function () {
+    
+    it("测试 #listToTree()", function(){
+        const list= [
+            {"id":1,"name":"通知公告","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":2,"name":"新闻动态","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":3,"name":"会议纪要","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":4,"name":"学习交流","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":5,"name":"视读空间","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":6,"name":"曝光专栏","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":7,"name":"Program","domain":"ebook","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":8,"name":"Java","domain":"ebook","pid":7,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":9,"name":"Java IO","domain":"ebook","pid":8,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+        ];
+        const tree=listToTree(list);
+
+        const program=tree[6];
+        assert.equal(program.value.name, list[6].name);
+
+        const java=program.children[0];
+        assert.equal(java.value.name, list[7].name);
+
+        const javaio=java.children[0];
+        assert.equal(javaio.value.name,list[8].name);
+    } );
+});
