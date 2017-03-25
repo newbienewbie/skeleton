4f99191e461faba35260eeb11a28eb0a8bc0d249
newbienewbie <webbot@sina.com>
2017-03-25 20:58:36 +0800

    后端：重构category - domain重命名为scope

--- test/backend/service/category/tree.test.js
+++ test/backend/service/category/tree.test.js
@@ -6,15 +6,15 @@ describe("测试 tree.js",function () {
     
     it("测试 #listToTree()", function(){
         const list= [
-            {"id":1,"name":"通知公告","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":2,"name":"新闻动态","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":3,"name":"会议纪要","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":4,"name":"学习交流","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":5,"name":"视读空间","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":6,"name":"曝光专栏","domain":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":7,"name":"Program","domain":"ebook","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":8,"name":"Java","domain":"ebook","pid":7,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
-            {"id":9,"name":"Java IO","domain":"ebook","pid":8,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":1,"name":"通知公告","scope":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":2,"name":"新闻动态","scope":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":3,"name":"会议纪要","scope":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":4,"name":"学习交流","scope":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":5,"name":"视读空间","scope":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":6,"name":"曝光专栏","scope":"post","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":7,"name":"Program","scope":"ebook","pid":null,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":8,"name":"Java","scope":"ebook","pid":7,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
+            {"id":9,"name":"Java IO","scope":"ebook","pid":8,"note":null,"createdAt":"2017-03-18 10:18:02","updatedAt":"2017-03-18 10:18:02"},
         ];
         const tree=listToTree(list);
 
