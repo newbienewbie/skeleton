c364f0a7a96ca8c443d79879137b0bfb94ce9f8d
newbienewbie <webbot@sina.com>
2017-03-31 17:29:51 +0800

    后端服务：comment-service增加like、hate等功能及测试

--- test/backend/service/comment/index.test.js
+++ test/backend/service/comment/index.test.js
@@ -41,4 +41,53 @@ describe('测试commentService',function(){
             });
     });
 
+    it('测试#create()、#like、#cancelLike和#remove()',function(){
+        const comment={
+            id:null,
+            content:'测试',
+            authorId:1,
+            topicId:1,
+            scope:'post',
+        };
+        // 创建，删除
+        return commentService.create(comment)
+            .then(c=>{
+                comment.id=c.id; //保存id到原comment对象
+                assert.equal(c.content,comment.content);
+                assert.equal(c.authorId,comment.authorId);
+                assert.equal(c.topicId,comment.topicId);
+                return c;
+            })
+            .then(c=>{
+                return commentService.like(comment.authorId,comment.id)
+            })
+            .then(_=>{
+                assert.ok(_,"必定不是false");
+                assert.ok(Array.isArray(_),"必定是Array");
+                assert.equal(_.length,2,"数组长度必定是2");
+                return commentService.findById(comment.id)
+                    .then(c=>{
+                        assert.ok(c.upvotes>0);
+                    });
+            })
+            .then(c=>{
+                return commentService.cancelLike(comment.authorId,comment.id);
+            })
+            .then(_=>{
+                console.log("******************",_);
+                assert.ok(_,"必定不是false");
+                assert.ok(Array.isArray(_),"必定是Array");
+                assert.equal(_.length,2,"数组长度必定是2");
+            })
+            .then(_=>{
+                return commentService.remove(comment.id)
+            })
+            .then(c=>{
+                return commentService.findById(comment.id)
+                    .then(c=>{
+                        assert.equal(c,null);
+                    });
+            });
+    });
+
 });
