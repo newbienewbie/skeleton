057c8c853794486d9c5475b5cf3d8a3ba96c5363
newbienewbie <webbot@sina.com>
2017-09-25 23:20:57 +0800

    后端：服务：添加获取指定用户角色的服务

--- backend/service/account/user-service.js
+++ backend/service/account/user-service.js
@@ -5,6 +5,14 @@ function findById(userId){
     return domain.user.findById(userId);
 }
 
+function getRoles(userId,roles){
+    return domain.user.findById(userId)
+        .then(user=>{
+            if(!user){throw new Error(`user not found :${userId}`)}
+            return user.getRoles();
+        });
+}
+
 
 module.exports={
     findById,
