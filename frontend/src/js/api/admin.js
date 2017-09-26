791e56e3657a0d6e169397dff1beb8708196b3ee
newbienewbie <webbot@sina.com>
2017-09-27 00:13:26 +0800

    前端：API：完善列举用户角色API的参数默认值

--- frontend/src/js/api/admin.js
+++ frontend/src/js/api/admin.js
@@ -18,9 +18,20 @@ export function createRole(name,description){
 }
 
 
-export function listRolesOfCurrentUser(){
+export function listRoles(page=1,size=8,condition={}){
     return client.post('/role/list',
-        {},
+        JSON.stringify({page,size,condition}),
+        {
+            headers:{
+                'Content-Type':'application/json',
+            },
+        }
+    ).then(resp=>resp.data,e=>{throw e;});
+}
+
+export function listRolesOfCurrentUser(page=1,size=8,condition={}){
+    return client.post('/role/list',
+        JSON.stringify({page,size,condition}),
         {
             headers:{
                 'Content-Type':'application/json',
