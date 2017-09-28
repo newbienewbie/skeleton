726c5a8a732bab70de9ba96a5b12b4bf8e5070cc
newbienewbie <webbot@sina.com>
2017-09-29 00:20:36 +0800

    前端：后台：角色：添加角色表单与后端交互的实现

--- frontend/src/js/components/admin/role/add.js
+++ frontend/src/js/components/admin/role/add.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import {Row,Col} from 'antd';
+import {Row,Col,message} from 'antd';
 import {AddOrEditForm} from './_common/add-or-edit-form';
+import {createRole} from '../../../api/admin';
 
 
 export class Add extends React.Component{
+    constructor(props){
+        super(props);
+        this.formRef=null;
+    }
 
     render() {
         return (<div>
-            <AddOrEditForm />
+            <AddOrEditForm ref={form=>this.formRef=form} onOk={_=>{
+                this.formRef.validateFields((err,value)=>{
+                    if(!err){
+                        const {name,description}=value;
+                        createRole(name,description)
+                            .then(resp=>{
+                                message.success(`创建成功`);
+                                this.formRef.resetFields();
+                            })
+                            .catch(e=>{
+                                message.error(`失败`+e);
+                            });
+                    }
+                });
+            }} />
         </div>);
     }
 }
