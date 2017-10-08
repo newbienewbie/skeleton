fb3d6c5c94a1326d476939bb30a3e2a46ed2b30e
newbienewbie <webbot@sina.com>
2017-10-08 16:53:43 +0800

    前端：后台：角色：列表页重构，只创建一次Datagrid

--- frontend/src/js/components/admin/role/list.js
+++ frontend/src/js/components/admin/role/list.js
@@ -6,100 +6,10 @@ import {datagrid} from "./adminui";
 import {model} from './_common/model';
 
 
+const DG=datagrid.create(model,AddOrEditFormModal);
+
 export class List extends React.Component{
     render(){
-        const DG=datagrid.create(model,AddOrEditFormModal);
         return <DG/> ;
     }
 }
-
-// export class List extends React.Component{
-
-//     constructor(props){
-//         super(props);
-//         this.state={
-//             data:[],
-//             pagination:{},
-//             loading:true,
-//             editModalVisible:false,
-//             currentRecord:{},
-//         };
-//         this.editForm=null;
-//     }
-
-//     onRemove(record){
-//         message.warning("hello,world"+record.name);
-//     }
-
-    
-
-//     componentDidMount(){
-//         this.setState({loading:true},()=>{
-//             const {current,pageSize,}=this.state.pagination;
-//             return listRoles(current,pageSize).then(roles=>{
-//                 const dataSource=roles.map((r,i)=>{
-//                     return Object.assign({},r,{key:i});
-//                 });
-//                 this.setState({data:dataSource,loading:false});
-//             });
-//         });
-//     }
-
-
-//     render() {
-//         const {Column,ColumnGroup}=Table;
-//         return (<div>
-//             <Table dataSource={this.state.data} pagination={this.state.pagination} loading={this.state.loading} onChange={this.state.onChange} >
-//                 <Column title="角色名" dataIndex="name" key="role_name" />
-//                 <Column title="角色描述" dataIndex="description" key="role_description" />
-//                 <Column title="操作" key="action" render={(text, record) => (
-//                     <span>
-//                         <a onClick={()=>{
-//                             this.setState({editModalVisible:true,currentRecord:record})
-//                             return false;
-//                         }}
-//                         >修改</a>
-//                         <span className="ant-divider" />
-//                         <Popconfirm title="确认要删除吗" okText="是" cancelText="否"
-//                             onConfirm={() => { this.onRemove(record); }}
-//                         >
-//                             <a href="#">删除</a>
-//                         </Popconfirm>
-//                         <span className="ant-divider" />
-//                     </span>)} />
-//             </Table>
-
-//             <AddOrEditFormModal ref={form=>this.editForm=form} visible={this.state.editModalVisible} 
-//                 initialValues={this.state.currentRecord}
-//                 onOk={_=>{
-//                     this.editForm.validateFields((err,values)=>{
-//                         if(!err){
-//                             const {id}=this.state.currentRecord;
-//                             const {name,description}=values;
-//                             updateRole(id,name,description)
-//                                 .then(resp=>{
-//                                     message.success(`修改成功`);
-//                                     console.log(resp);
-//                                     this.setState({editModalVisible:false},()=>{
-//                                         // 刷新数据源
-//                                         const {current,pageSize,}=this.state.pagination;
-//                                         return listRoles(current,pageSize)
-//                                             .then(roles=>{
-//                                                 const dataSource=roles.map((r,i)=>{
-//                                                     return Object.assign({},r,{key:i});
-//                                                 });
-//                                                 this.setState({data:dataSource,loading:false});
-//                                             });
-//                                     });
-//                                 })
-//                         }
-//                     });
-//                 }}
-//                 onCancel={_=>{
-//                     this.setState({editModalVisible:false});
-//                 }}
-//             />
-
-//         </div>);
-//     }
-// }
