42077d2dd5bf68addb611cc0329049948dc5a2aa
newbienewbie <webbot@sina.com>
2017-10-08 23:07:10 +0800

    前端：后台：分离adminui内容到单独的组件tiny-admin中

--- frontend/src/js/components/admin/role/add.js
+++ frontend/src/js/components/admin/role/add.js
@@ -2,11 +2,9 @@ import React from 'react';
 import {Row,Col,message} from 'antd';
 import {PlainAddOrEditForm} from './_common/add-or-edit-form';
 import {model} from './_common/model';
-import {defaultDecoratedForm,addform} from "./adminui";
+import {defaultDecoratedForm,addform} from 'tiny-admin';
 
-const AddOrEditForm=defaultDecoratedForm.createDecoratedAddOrEditForm(PlainAddOrEditForm);
 
-const AddForm=addform.create(model,AddOrEditForm);
 
 export class Add extends React.Component{
 
@@ -15,6 +13,8 @@ export class Add extends React.Component{
     }
 
     render(){
+        const AddOrEditForm=defaultDecoratedForm.createDecoratedAddOrEditForm(PlainAddOrEditForm);
+        const AddForm=addform.create(model,AddOrEditForm);
         return <AddForm/>;
     }
 }
--- frontend/src/js/components/admin/role/list.js
+++ frontend/src/js/components/admin/role/list.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {Row,Col,Button,Table,Modal,Popconfirm,message} from 'antd';
 import {listRoles,listRolesOfCurrentUser,updateRole} from '../../../api/admin';
 import {PlainAddOrEditForm} from './_common/add-or-edit-form';
-import {defaultDecoratedForm,datagrid} from "./adminui";
 import {model} from './_common/model';
+import {defaultDecoratedForm,datagrid} from 'tiny-admin';
 
 
-const AddOrEditFormModal=defaultDecoratedForm.createDecoratedAddOrEditFormModal(PlainAddOrEditForm);
-const DG=datagrid.create(model,AddOrEditFormModal);
 
 export class List extends React.Component{
     render(){
+        const AddOrEditFormModal=defaultDecoratedForm.createDecoratedAddOrEditFormModal(PlainAddOrEditForm);
+        const DG=datagrid.create(model,AddOrEditFormModal);
         return <DG/> ;
     }
 }
