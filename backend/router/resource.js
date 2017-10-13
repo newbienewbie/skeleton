8a08b1e490248450b04125bfe80edf57822b65e9
newbienewbie <webbot@sina.com>
2017-10-13 15:42:48 +0800

    后端：使用tiny-service创建资源模型的服务、路由

---
+++ backend/router/resource.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const resourceService=require('../service/resource');
+const {Middleware}=require('tiny-service');
+
+
+
+const router=express.Router();
+const middleware=Middleware(resourceService);
+
+
+router.post('/create',bodyParser.json(),middleware.create);
+
+
+router.post('/remove',bodyParser.json(),middleware.remove);
+
+
+router.post('/update',bodyParser.json(),middleware.update);
+
+
+router.post('/list',bodyParser.json(),middleware.list);
+
+
+router.post('/recent',bodyParser.json(),middleware.recent);
+
+
+module.exports=router;
---
+++ backend/service/resource/index.js
@@ -0,0 +1,9 @@
+const {Service}=require('tiny-service');
+const domain=require('../../domain');
+
+
+const resourceService=Service(domain.resource);
+
+
+
+module.exports=resourceService;
