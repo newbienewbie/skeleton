dccbe763034c2d0cb3d952aeff5c79aa9c9b0fb6
newbienewbie <webbot@sina.com>
2017-10-15 20:41:05 +0800

    后端：路由：资源路由：添加与角色相关的路由

--- backend/router/resource.js
+++ backend/router/resource.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const resourceService=require('../service/account/resource-service');
-const {Middleware}=require('tiny-service');
+const {Middleware,message}=require('tiny-service');
 
 
 
@@ -22,6 +22,70 @@ router.post('/list',bodyParser.json(),middleware.list);
 
 
 router.post('/recent',bodyParser.json(),middleware.recent);
+
+
+
+/**
+ * 创建资源，并关联角色
+ */
+router.post('/create-resource-of-role',bodyParser.json(),function(req,res){
+    const {record,context}=req.body;
+    const {headItem}=context;
+    const roleId=headItem.id;
+
+    resourceService.createResourceOfRole(roleId,record)
+        .then(
+            _=>{
+                res.json(message.success());
+            },
+            r=>{
+                res.json(message.fail(r));
+            }
+        );
+});
+
+/**
+ * 只移除关联，并不移除角色或者资源
+ */
+router.post('/remove-resource-of-role',bodyParser.json(),function(req,res){
+    const {record,context}=req.body;
+    const {headItem}=context;
+    if(!headItem.id){
+        res.json(message.fail('context.headItem.id required'));
+        return;
+    }
+
+    const {id}=record;
+    const roleId=headItem.id;
+    resourceService.removeResourceOfUser(roleId,{id})
+        .then(
+            _=>{
+                res.json(message.success());
+            },
+            r=>{
+                res.json(message.fail(r));
+            }
+        );
+});
+
+
+
+router.post('/list-resources-of-role',bodyParser.json(),function(req,res){
+    const {page,size,condition,context}=req.body;
+    if(!context || !context.headItem ||!context.headItem.id){
+        res.json({
+            counts:0,
+            rows:[],
+        });
+        return;
+    }
+    const roleId=context.headItem.id;
+    return resourceService.listResourcesOfRole(roleId,page,size,condition)
+        .then(resources=>{
+            res.json(resources);
+        });
+});
+
 
 
 module.exports=router;
