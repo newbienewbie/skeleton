74fe6dd419446ca42df9e9ef0e60fa170e753b07
newbienewbie <webbot@sina.com>
2017-10-18 18:34:30 +0800

    前端：后台：api：修复文章创建过程时payload过大问题

--- frontend/src/js/api/admin/post.js
+++ frontend/src/js/api/admin/post.js
@@ -15,5 +15,17 @@ const defaultClientOpts= {
 postapi.detail=function(id){
     return client.get(`/post/detail?id=${id}`)
         .then(resp=>resp.data,e=>{throw e;});
-}
+};
 
+const _create=postapi.create;
+postapi.create=function(record,context){
+    let {featureImageUrl}=record;
+    featureImageUrl=featureImageUrl.map(i=>{
+        const {lastModified, lastModifiedDate, name,
+            originFileObj, percent, response, size, type, uid, url}=i;
+        return {lastModified, lastModifiedDate, name,
+            originFileObj, percent, response, size, type, uid, url};
+    });
+    record.featureImageUrl=featureImageUrl;
+    return _create(record,context);
+}
