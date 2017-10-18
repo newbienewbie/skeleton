a09f1c94b278c55f8ed827155884e333d54c5930
newbienewbie <webbot@sina.com>
2017-10-18 20:16:45 +0800

    前端：后台：keyword-selector现在支持受控模式

--- frontend/src/js/components/admin/utils/keyword-selector.js
+++ frontend/src/js/components/admin/utils/keyword-selector.js
@@ -1,34 +1,52 @@
 import React from  'react';
 import {Select} from  'antd';
+import {fixControlledValue} from './helper';
 
-export const KeywordSelector=React.createClass({
 
-    getDefaultProps:function(){
-        return {
-            keywords:[],
-            onChange:(list)=>{},
+export class KeywordSelector extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            value:this._getValue(this.props.value),
         };
-    },
+    }
 
-    _getValue(){
-        const keywords=this.props.keywords;
+    componentWillReceiveProps(nextProps){
+        if( 'value' in nextProps){
+            const nextValue=fixControlledValue(nextProps.value);
+            const thisValue=fixControlledValue(this.props.value);
+            // 如果下一个value值和现在的value值相同，则不再同步。
+            if(nextValue == thisValue){
+                return;
+            }
+            this.setState({ value:nextValue });
+        }
+    }
+
+    _getValue(keywords){
         if(!!keywords&& !!(keywords[0]) && !!(keywords[0].tag)){
             const value=keywords.map(kw=>kw.tag);
             return value;
         }
         return [];
-    },
+    }
 
-    render:function(){
+    render(){
         return (
-        <Select tags={true}  searchPlaceholder="关键词" 
-            value={this._getValue()}
+        <Select mode={'tags'}  searchPlaceholder="关键词" 
+            value={this._getValue(this.props.value)}
             onChange={this.props.onChange}
         >
         </Select>);
-    },
+    }
 
-});
+}
+
+KeywordSelector.defaultProps={
+    value:[],
+    onChange:(list)=>{},
+};
 
 
 export default KeywordSelector;
