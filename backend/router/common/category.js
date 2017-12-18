361b6546fb8b2f0ecc8753020b71c797c27d49d8
newbienewbie <webbot@sina.com>
2017-12-19 00:40:25 +0800

    后端：路由：使用集中式声明重构category路由

--- backend/router/common/category.js
+++ backend/router/common/category.js
@@ -4,22 +4,43 @@ const {categoryService}=require('../../service');
 
 const router=express.Router();
 
-router.use('/list/:scope',(req,res,next)=>{
+const routes={
+    'list-of-scope':{
+        method:'use',
+        path:'/list/:scope',
+        middlewares:[ listOfScope ],
+    },
+    'tree-of-scope':{
+        method:'use',
+        path:'/tree/:scope',
+        middlewares:[ treeOfScope ],
+    },
+};
+
+function listOfScope(req,res,next){
     const scope=req.params.scope?req.params.scope:"post";
     categoryService.listAll({
         'scope':scope
     }).then(list=>{
         res.end(JSON.stringify(list));
     });
-});
+}
 
-router.use('/tree/:scope',(req,res,next)=>{
+function treeOfScope(req,res,next){
     const scope=req.params.scope?req.params.scope:"post";
     categoryService.tree({
         'scope':scope
     }).then(list=>{
         res.end(JSON.stringify(list));
     });
+}
+
+Object.keys(routes).forEach(k=>{
+    const {method,path,middlewares}=routes[k];
+    middlewares.forEach(mw=>{
+        router[method](path,mw);
+    });
 });
+
 
 module.exports=router;
