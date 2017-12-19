a1d58cff1ffae3810e1812e130fb198c1803a9f4
newbienewbie <webbot@sina.com>
2017-12-19 16:44:12 +0800

    后端：路由：重构路由文件为集中式声明

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const path=require('path');
+const routesConfig=require('./routes.config');
 
+/**
+ * 注册单个Route文件
+ * @param {*} app 
+ * @param {String} filePath 文件路径
+ */
 function registerRouteFile(app,filePath){
 
     const router=express.Router();
@@ -19,60 +25,14 @@ function registerRouteFile(app,filePath){
 
 function register(app){
 
-
     /////////////// session
     app.use('/',require('./session'));
 
-    /////////////// pages
-    [
-        './page/home.js',
-        './page/dashboard.js',    // dashboard page
-        './page/about',          // about page
-        './page/contact',        // contact page
-        './page/not-found.js',   // 404 page
-    ].forEach(p=> registerRouteFile(app,p));
-
-    /////////////// 超级管理模块
-    [
-        './sudo/health',
-        './sudo/info',
-        './sudo/install',
-    ].forEach(p=> registerRouteFile(app,p));
-
-
-    /////////////// 系统管理相关模块
-    [
-        './system/account',
-        './system/role',
-        './system/resource',
-    ].forEach(p=> registerRouteFile(app,p));
-
-
-    /////////////// common
-    [
-        './common/upload.js',    // 上传
-        './common/ueditor.js',   // 编辑器
-        './common/category',     // category
-        './common/country.js',   // country
-        './common/language.js',  // language
-    ].forEach(p=> registerRouteFile(app,p));
-
-
-
-    /////////////// cms 模块
-    [
-        './cms/movie',      // movie 模块
-        './cms/post',       // post 模块
-        './cms/ebook',      // ebook 模块
-        './cms/director',   // director 模块
-    ].forEach(p=> registerRouteFile(app,p));
-
-
-
-    [
-        './comment'         // 评论回复模块
-    ].forEach(p=> registerRouteFile(app,p));
-
+    Object.keys(routesConfig).forEach(k=>{
+        const config=routesConfig[k];
+        const {category,files}=config;
+        files.forEach(p=>registerRouteFile(app,p));
+    });
 
     return app;
 }
---
+++ backend/router/routes.config.js
@@ -0,0 +1,67 @@
+const path=require('path');
+
+let routesConfig={
+    'page':{
+        category:'页面',
+        files:[
+            './page/home.js',
+            './page/dashboard.js',    // dashboard page
+            './page/about',          // about page
+            './page/contact',        // contact page
+            './page/not-found.js',   // 404 page
+        ],
+    },
+    'sudo':{
+        category:'系统',
+        files:[
+            './sudo/health',
+            './sudo/info',
+            './sudo/install',
+        ],
+    },
+    'system':{
+        category:'系统',
+        files:[
+            './system/account',
+            './system/role',
+            './system/resource',
+        ],
+    },
+    'common':{
+        category:'通用',
+        files:[
+            './common/upload.js',    // 上传
+            './common/ueditor.js',   // 编辑器
+            './common/category',     // category
+            './common/country.js',   // country
+            './common/language.js',  // language
+        ],
+    },
+    'cms':{
+        category:'领域',
+        files:[
+            './cms/movie',      // movie 模块
+            './cms/post',       // post 模块
+            './cms/ebook',      // ebook 模块
+            './cms/director',   // director 模块
+        ],
+    },
+    'comment':{
+        category:'领域',
+        files:[
+            './comment',        // 评论回复模块
+        ],
+    },
+};
+
+
+// 转换为绝对路径
+Object.keys(routesConfig).forEach(rcName=>{
+    const rc=routesConfig[rcName];
+    const {files}=rc;
+    files.forEach((p,idx)=>{
+        files[idx]= path.resolve(__dirname,p);
+    });
+});
+
+module.exports=routesConfig;
