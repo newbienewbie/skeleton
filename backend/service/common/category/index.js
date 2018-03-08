1adfeb53de505a4255418bfcb159bcfb3b4673a4
xu lou <itminus@hotmail.com>
2018-03-08 08:47:45 +0800

    后端：服务：将category-service分离到tiny-service中

--- backend/service/common/category/index.js
+++ backend/service/common/category/index.js
@@ -1,41 +1,8 @@
 const domain=require('../../../domain');
-const {listToTree,subnodeIdList} =require('./tree');
+const {CategoryService}=require('tiny-service');
 
-function list(page=1,size=100,condition={}){
-    return domain.category.findAndCount({
-        offset:(page-1)*size,
-        limit:size,
-        where:condition,
-    });
-}
+const categoryService=CategoryService(domain.category);
 
-function listAll(condition={}){
-    return domain.category.findAndCount({where:condition});
-}
+console.log(categoryService);
 
-
-/**
- * 根据condition晒出一批category，然后从中选出以pid为祖先的节点，然后返回这些节点的id列表
- * condition可以用来指定scope等条件
- */
-function getCategorySubnodeIdList(pid,condition={}){
-    return domain.category.findAll({where:condition})
-        .then(list=>{
-            if(list){
-                return subnodeIdList(list,pid);
-            }else{
-                return [];
-            }
-        });
-}
-
-
-function tree(condition={}){
-    return domain.category.findAll({where:condition})
-        .then(list=>listToTree(list));
-}
-
-
-module.exports={
-    list,listAll,getCategorySubnodeIdList,tree,
-};
+module.exports=categoryService;
